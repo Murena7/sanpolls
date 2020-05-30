0caf7b073757ea90fca12adda0f20bda5ec1cdc7
dima-rm <dima_rm@yahoo.de>
2020-05-30 14:53:29 +0000

    First commit. Adding some components

--- client/src/app/shared/material/material.module.ts
+++ client/src/app/shared/material/material.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatButtonModule],
-  exports: [MatButtonModule],
+  imports: [CommonModule, MatButtonModule, MatDialogModule, MatFormFieldModule],
+  exports: [MatButtonModule, MatDialogModule, MatFormFieldModule],
 })
 export class MaterialModule {}
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarModule } from './navbar/navbar.module';
 import { ComponentsModule } from './components/components.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, NavbarModule, ComponentsModule],
+  imports: [CommonModule, NavbarModule, ComponentsModule, FormsModule, ReactiveFormsModule],
   exports: [NavbarModule, ComponentsModule],
 })
 export class SharedModule {}
