ee6c34c07741fcaec3631cb89931d703d152073e
Andrei Burmitskii <anburan@gmail.com>
2020-07-09 01:52:20 +0300

    Some Deploy Fix 2

--- api/src/types/express/index.d.ts
+++ api/src/types/express/index.d.ts
@@ -1,12 +1,12 @@
 import { User as CurrentUser } from '../../entity/user';
 
 declare global {
-  namespace Express {
-    export interface Request {
-      // @ts-ignore
-      user: CurrentUser;
-    }
-  }
+  // namespace Express {
+  //   export interface Request {
+  //     // @ts-ignore
+  //     user: CurrentUser;
+  //   }
+  // }
 
   namespace Models {
     export type UserModel = CurrentUser;
