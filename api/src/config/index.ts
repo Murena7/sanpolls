06d89c5a35b0ebcc919b495081bbdbd07b3de36c
Andrei Burmitskii <anburan@gmail.com>
2020-07-09 03:10:50 +0300

    Some Deploy Fix 5

--- api/src/config/index.ts
+++ api/src/config/index.ts
@@ -3,11 +3,13 @@ import dotenv from 'dotenv';
 // Set the NODE_ENV to 'development' by default
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-const envFound = dotenv.config();
-if (envFound.error) {
-  // This error should crash whole process
+if (process.env.NODE_ENV === 'development') {
+  const envFound = dotenv.config();
+  if (envFound.error) {
+    // This error should crash whole process
 
-  throw new Error("⚠️  Couldn't find .env file  ⚠️");
+    throw new Error("⚠️  Couldn't find .env file  ⚠️");
+  }
 }
 
 export default {
