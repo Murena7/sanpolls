7a7646d6ba05e263066969aa64e422d00e5d2135
dima-rm <dima_rm@yahoo.de>
2020-08-13 07:40:09 +0000

    Moving of ngx simple countdown to the shared directives folder. Adding of Comments component and Likes buttons checking on Polls page

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { NavbarModule } from './navbar/navbar.module';
-import { ComponentsModule } from '@components/components.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { PipesModule } from '@shared/pipes/pipes.module';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {NavbarModule} from './navbar/navbar.module';
+import {ComponentsModule} from '@components/components.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {PipesModule} from '@shared/pipes/pipes.module';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {SatPopoverModule} from '@ncstate/sat-popover';
 
 @NgModule({
   declarations: [],
@@ -16,7 +17,9 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     ReactiveFormsModule,
     PipesModule,
     FontAwesomeModule,
+    SatPopoverModule
   ],
-  exports: [NavbarModule, ComponentsModule, FormsModule, ReactiveFormsModule, PipesModule, FontAwesomeModule],
+  exports: [NavbarModule, ComponentsModule, FormsModule, ReactiveFormsModule, PipesModule, FontAwesomeModule, SatPopoverModule],
 })
-export class SharedModule {}
+export class SharedModule {
+}
