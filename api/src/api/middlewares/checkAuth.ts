44e4a2db0e42e02fe7315039d0341efdf1028cb3
Andrei Burmitskii <anburan@gmail.com>
2020-09-02 01:47:14 +0300

    Add unauthorized message

--- api/src/api/middlewares/checkAuth.ts
+++ api/src/api/middlewares/checkAuth.ts
@@ -3,14 +3,15 @@ import { UserStatus } from '../../interfaces/user';
 
 const checkAuth = (roles?: string[]) => {
   return (req, res: Response, next) => {
+    const message = 'Unauthorized';
     if (!req.isAuthenticated()) {
-      return res.status(401).send();
+      return res.status(401).send({ message });
     }
 
     if (req.user.status === UserStatus.Inactive) {
       //PassportJS Logout
       req.logout();
-      return res.status(401).send();
+      return res.status(401).send({ message });
     }
 
     if (!roles) {
@@ -20,7 +21,7 @@ const checkAuth = (roles?: string[]) => {
     if (roles.indexOf(req.user.role) > -1) {
       return next();
     } else {
-      return res.status(401).send();
+      return res.status(401).send({ message });
     }
   };
 };
